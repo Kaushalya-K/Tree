202f19e5859db12b76dd4ad84fff86d17997fddb
Kaushalya-K <66408688+Kaushalya-K@users.noreply.github.com>
2020-06-04 13:32:52 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   fun(3);
+   return 0;
+}
+void fun(int n)
+{
+
+    if(n>0)
+    {
+
+        printf("%d ",n);
+        fun(n-1);
+        fun(n-1);
+    }
+}
